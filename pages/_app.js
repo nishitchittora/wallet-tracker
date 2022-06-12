56826d1c8547647b58fbf34900da2bb4cd72561d
Nishit Chittora <nishitchittora2015@gmail.com>
2022-06-12 18:34:24 +0530

    Skeletion added

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <ChakraProvider> <MoralisProvider appId={process.env.NEXT_PUBLIC_APPID} serverUrl={process.env.NEXT_PUBLIC_SERVERURL}> <Component {...pageProps} /></MoralisProvider></ChakraProvider>
+    return (
+        <ChakraProvider>
+            <MoralisProvider
+                appId={process.env.NEXT_PUBLIC_APPID}
+                serverUrl={process.env.NEXT_PUBLIC_SERVERURL}
+            >
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </ChakraProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
