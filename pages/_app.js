2eda7cbde7eaa8560a1a94cee8b67c45ee391dbf
Nishit Chittora <nishitchittora2015@gmail.com>
2022-06-12 15:56:11 +0530

    Initialized

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ChakraProvider> <MoralisProvider appId={process.env.NEXT_PUBLIC_APPID} serverUrl={process.env.NEXT_PUBLIC_SERVERURL}> <Component {...pageProps} /></MoralisProvider></ChakraProvider>
 }
 
 export default MyApp
